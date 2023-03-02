25f9b4f2ebb469e68621c1bcc1698a7c3a45269c
hugo1623 <hugoluis.system@gmail.com>
2023-03-02 02:12:12 -0500

    Creation of the Dto DiscForListDto

---
+++ UniversalMusic.Management.Application/Dtos/DiscForListDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalMusic.Management.Application.Dtos
+{
+    internal class DiscForListDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Artist { get; set; }
+
+        public string GenreType { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public int TotalSales { get; set; }
+
+        public bool IsTopTen { get; set; }
+    }
+}
